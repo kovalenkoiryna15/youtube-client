bc2a2c1ea2acf1c719aae547973c4be1b460249a
Iryna Kavalenka <kovalenkoiryna15@gmail.com>
2021-07-28 16:53:48 +0300

    feat: created card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,16 @@ import AppComponent from './app.component';
 import HeaderComponent from './components/header/header.component';
 import SearchComponent from './components/header/search/search.component';
 import SearchResultComponent from './components/search-result/search-result.component';
+import CardComponent from './components/search-result/card/card.component';
 
-function components() {
-  return [
+@NgModule({
+  declarations: [
     AppComponent,
     HeaderComponent,
     SearchComponent,
     SearchResultComponent,
-  ];
-}
-
-@NgModule({
-  declarations: [...components()],
+    CardComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
