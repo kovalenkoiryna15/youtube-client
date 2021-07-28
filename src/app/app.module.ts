59baf770175598a51eb086951b2d2322b7f99d4a
Iryna Kavalenka <kovalenkoiryna15@gmail.com>
2021-07-28 18:54:36 +0300

    feat: created login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import AppRoutingModule from './app-routing.module';
+import MaterialModule from './material.module';
 
 import AppComponent from './app.component';
 import HeaderComponent from './components/header/header.component';
 import SearchComponent from './components/search/search.component';
 import SearchResultComponent from './components/search-result/search-result.component';
 import CardComponent from './components/card/card.component';
+import LoginComponent from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,8 +19,14 @@ import CardComponent from './components/card/card.component';
     SearchComponent,
     SearchResultComponent,
     CardComponent,
+    LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports: [MatIconModule, MatInputModule],
+  exports: [MatIconModule, MatInputModule],
+  providers: [],
+})
+export default class MaterialModule {}
