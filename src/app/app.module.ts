370e3184ea242ce6b361c8e5d3aacca580a6d1d3
Iryna Kavalenka <kovalenkoiryna15@gmail.com>
2021-07-28 16:04:49 +0300

    feat: created header, search and search-result components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
+
 import AppComponent from './app.component';
+import HeaderComponent from './components/header/header.component';
+import SearchComponent from './components/header/search/search.component';
+import SearchResultComponent from './components/search-result/search-result.component';
+
+function components() {
+  return [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchResultComponent,
+  ];
+}
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [...components()],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
