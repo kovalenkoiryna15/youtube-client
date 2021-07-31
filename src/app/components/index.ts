51082106bfcbfb789c1b01a1692624668cdf1b25
Iryna Kavalenka <kovalenkoiryna15@gmail.com>
2021-07-31 15:15:15 +0300

    added filter and settings components

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,6 +4,8 @@ import SearchResultComponent from './search-result/search-result.component';
 import SearchItemComponent from './search-result/search-item/search-item.component';
 import LoginComponent from './header/login/login.component';
 import LogoComponent from './header/logo/logo.component';
+import FilterComponent from './header/filter/filter.component';
+import SettingsComponent from './header/settings/settings.component';
 
 const components = [
   HeaderComponent,
@@ -12,6 +14,8 @@ const components = [
   SearchItemComponent,
   LoginComponent,
   LogoComponent,
+  FilterComponent,
+  SettingsComponent,
 ];
 
 export default components;
